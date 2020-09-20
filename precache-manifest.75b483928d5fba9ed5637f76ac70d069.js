bd87a7af4c0ba4ee831f9c578bd174ca85a9f79c
SerhiiHryn <greenserhii@gmail.com>
2020-09-20 17:59:42 +0300

    Updates

---
+++ precache-manifest.75b483928d5fba9ed5637f76ac70d069.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0324965bc97ebc47a7464b938833ddbb",
+    "url": "/homer-timeline/index.html"
+  },
+  {
+    "revision": "cabb3c468ca4ffe02751",
+    "url": "/homer-timeline/static/css/main.c8f52f23.chunk.css"
+  },
+  {
+    "revision": "3887695e5466cc0b767e",
+    "url": "/homer-timeline/static/js/2.986554e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homer-timeline/static/js/2.986554e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cabb3c468ca4ffe02751",
+    "url": "/homer-timeline/static/js/main.90151f85.chunk.js"
+  },
+  {
+    "revision": "70f3e47bceaf111f524d",
+    "url": "/homer-timeline/static/js/runtime-main.89fb6010.js"
+  },
+  {
+    "revision": "c3e113c19f2c0831dc450413babadd70",
+    "url": "/homer-timeline/static/media/add-event-button.c3e113c1.svg"
+  },
+  {
+    "revision": "0e9f86f203fead3e9318dc5cd6d6d057",
+    "url": "/homer-timeline/static/media/thumbnail.0e9f86f2.jpg"
+  }
+]);
